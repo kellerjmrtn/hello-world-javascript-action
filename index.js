3e0bf4c9725fb31542d0e757d9e0a99907eecdf5
Keller <kellerjmrtn@gmail.com>
2020-03-05 19:45:50 -0500

    simple-git test

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const simpleGit = require("simple-git");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  const repoToken = core.getInput("repo-token");
-  console.log(`Test ${repoToken}`);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+
+  /* Here is my code -------------------------- */
+  const repoToken = core.getInput("repo-token");
+  const client = new github.GitHub(repoToken);
+  const rawPayload = github.context.payload;
+  simpleGit.diff(null, (err, result) => {
+
+    // err is null unless this command failed
+    // result is the raw output of this command
+    console.log(err);
+    console.log(result);
+  
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
